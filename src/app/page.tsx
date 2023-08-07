750b4fd1f26f264c7997f4c12641e73b7ea0eea1
Bard <59461181+prove-ability@users.noreply.github.com>
2023-08-08 01:41:25 +0900

    feat: input 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
 import { supabase } from '@/lib/suparbase'
 
 async function getData() {
@@ -5,7 +9,31 @@ async function getData() {
   return todos
 }
 
-export default async function Page() {
-  const todos = await getData()
-  return <pre>{JSON.stringify(todos, null, 2)}</pre>
+export default function Page() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [todos, setTodos] = useState<any[] | null>(null)
+  useEffect(() => {
+    ;(async () => {
+      const data = await getData()
+      setTodos(data)
+    })()
+  }, [])
+  const [value, setValue] = useState('')
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
+  }
+
+  return (
+    <div>
+      <p>{JSON.stringify(todos, null, 2)}</p>
+      <div>
+        <form action="">
+          <label htmlFor="value">
+            <input type="text" value={value} onChange={handleChange} />
+          </label>
+          <button type="submit">추가</button>
+        </form>
+      </div>
+    </div>
+  )
 }
